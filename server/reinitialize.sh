2ad122dff6d772e2c7c7de51e8919012950d444a
Taylor Goolsby <taylorgoolsby@users.noreply.github.com>
2020-04-27 16:27:34 -0700

    fix bash pathing

--- server/reinitialize.sh
+++ server/reinitialize.sh
@@ -1,7 +1,10 @@
 
-cd server
+full_path=$(realpath $0)
+dir_path=$(dirname $full_path)
+echo $dir_path
+temp_dir="$dir_path/temp"
+echo $temp_dir
 rm -rf ./data/mysql/*
 rm -rf ./binlog/*
-tmpdir="`cd temp;pwd`"
-./mysqld --defaults-file=my.cnf --tmpdir=${tmpdir} --explicit_defaults_for_timestamp --log_syslog=0 --initialize-insecure
-./mysqld --defaults-file=my.cnf --tmpdir=${tmpdir} --explicit_defaults_for_timestamp --log_syslog=0 --console
+./mysqld --defaults-file=my.cnf --tmpdir=$temp_dir --explicit_defaults_for_timestamp --log_syslog=0 --initialize-insecure
+./mysqld --defaults-file=my.cnf --tmpdir=$temp_dir --explicit_defaults_for_timestamp --log_syslog=0 --console
