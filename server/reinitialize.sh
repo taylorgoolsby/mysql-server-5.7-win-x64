60fd3471b5cc7653fd3a5e340ac7d177ab8789a9
Taylor Goolsby <taylorgoolsby@users.noreply.github.com>
2020-04-27 16:31:26 -0700

    fix bash pathing

--- server/reinitialize.sh
+++ server/reinitialize.sh
@@ -1,10 +1,8 @@
 
 full_path=$(realpath $0)
 dir_path=$(dirname $full_path)
-echo $dir_path
 temp_dir="$dir_path/temp"
-echo $temp_dir
 rm -rf ./data/mysql/*
 rm -rf ./binlog/*
-./mysqld --defaults-file=my.cnf --tmpdir=$temp_dir --explicit_defaults_for_timestamp --log_syslog=0 --initialize-insecure
-./mysqld --defaults-file=my.cnf --tmpdir=$temp_dir --explicit_defaults_for_timestamp --log_syslog=0 --console
+$dir_path/mysqld --defaults-file=$dir_path/my.cnf --tmpdir=$temp_dir --explicit_defaults_for_timestamp --log_syslog=0 --initialize-insecure
+$dir_path/mysqld --defaults-file=$dir_path/my.cnf --tmpdir=$temp_dir --explicit_defaults_for_timestamp --log_syslog=0 --console
--- server/start.sh
+++ server/start.sh
@@ -1,8 +1,6 @@
     
 full_path=$(realpath $0)
 dir_path=$(dirname $full_path)
-echo $dir_path
 temp_dir="$dir_path/temp"
-echo $temp_dir
-./mysqld --defaults-file=my.cnf --tmpdir=$temp_dir --explicit_defaults_for_timestamp --log_syslog=0 --console
+$dir_path/mysqld --defaults-file=$dir_path/my.cnf --tmpdir=$temp_dir --explicit_defaults_for_timestamp --log_syslog=0 --console
 
